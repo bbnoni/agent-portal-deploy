e0b73c559614b6bfbe1f2f06eff1a7e3655efc53
benoniokaikoi <derhain@gmail.com>
2025-04-18 07:52:49 +0000

    🚀 New build 2025-04-18 07:52:49

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3204537897"
+    serviceWorkerVersion: "2058204426"
   }
 });
